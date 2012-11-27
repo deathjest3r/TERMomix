c391dc9ef9d434e30cbc630e28e994895015a15b
deathjest3r <death.jester@web.de>
2012-11-27 11:58:36 +0100

    Added GPL header to termomix.h file

--- include/termomix.h
+++ include/termomix.h
@@ -1,3 +1,27 @@
+/*******************************************************************************
+ *  Filename: termomix.h
+ *  Description: VTE-based terminal emulator
+ *               (Lots of code is taken from Sakura terminal application)
+ *
+ *           Copyright (C) 2012       Julian Vetter <death.jester@web.de>
+ *           Copyright (C) 2006-2012  David Gómez <david@pleyades.net>
+ *           Copyright (C) 2008       Hong Jen Yee (PCMan) <pcman.tw@gmail.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License version 2 as
+ *  published by the Free Software Foundation
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *****************************************************************************/
+
 #ifndef __TERMOMIX_H__
 #define __TERMOMIX_H__
 
